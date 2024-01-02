57806ac6b1e9cbdb8d82af1a69048d62da59754c
Sergey Kasmy <SleeplessSloth79@gmail.com>
2024-01-02 19:46:24 +0100

    Properly mark --ignore as a min only option

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::{collections::HashSet, process::Command};
 
 use argh::FromArgs;
 use color_eyre::{
@@ -14,10 +14,6 @@ struct Args {
     /// action
     #[argh(subcommand)]
     action: Option<Action>,
-
-    /// ignore
-    #[argh(option)]
-    ignore: Option<String>,
 }
 
 #[derive(FromArgs, Debug)]
@@ -35,9 +31,13 @@ struct All {}
 /// min
 #[derive(FromArgs, Debug)]
 #[argh(subcommand, name = "min")]
-struct Min {}
+struct Min {
+    /// ignore
+    #[argh(option)]
+    ignore: Option<String>,
+}
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Hash, PartialEq, Eq, Debug)]
 struct Device {
     device_type: String,
     free: u64,
@@ -59,21 +59,27 @@ struct Device {
 
 fn main() -> Result<()> {
     let args: Args = argh::from_env();
-    let act = args.action.unwrap_or(Action::Min(Min {}));
+    let act = args
+        .action
+        .unwrap_or_else(|| Action::Min(Min { ignore: None }));
 
     let duf = Command::new("duf")
         .arg("-json")
         .output()
         .wrap_err("Couldn't open duf")?;
 
-    let devices = match serde_json::from_slice::<Vec<Device>>(&duf.stdout) {
-        Ok(mut devices) => {
+    let devices = match serde_json::from_slice::<HashSet<Device>>(&duf.stdout) {
+        Ok(devices) => {
+            let mut devices = devices
+                .into_iter()
+                .filter(|x| {
+                    x.device_type == "local" && x.fs_type != "ramfs" && x.fs_type != "autofs"
+                })
+                .collect::<Vec<_>>();
+
             devices.sort_by_key(|x| x.free);
 
             devices
-                .into_iter()
-                .filter(|x| x.device_type == "local" && x.fs_type != "ramfs")
-                .collect::<Vec<_>>()
         }
         Err(e) => {
             bail!(
@@ -94,11 +100,11 @@ fn main() -> Result<()> {
                 println!("{}: {}", dev.mount_point, format_size(dev.free, BINARY));
             }
         }
-        Action::Min(_) => {
+        Action::Min(Min { ignore }) => {
             let min = devices
                 .iter()
                 .filter(|dev| {
-                    let Some(ignore) = &args.ignore else {
+                    let Some(ignore) = &ignore else {
                         return true;
                     };
 
